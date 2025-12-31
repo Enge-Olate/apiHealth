3eba01849db2710156f5d0f009ca6308ebed3b10
Enge-Olate <marcioalexisolate@live.com>
2025-12-31 15:09:04 -0300

    Finalizando testes.

--- profissionais/tests.py
+++ profissionais/tests.py
@@ -1,3 +1,39 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.urls import reverse
+from django.contrib.auth.models import User
+from .models import Profissional
+from rest_framework_simplejwt.tokens import RefreshToken
 
-# Create your tests here.
+class ProfissionalTests(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='test_user', password='pass123')
+        refresh = RefreshToken.for_user(self.user)
+        self.access_token = str(refresh.access_token)
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.access_token)        
+        self.url_list = reverse('profissional-list')
+        
+        self.dados = {
+            "nome": "Dr. Teste",
+            "crm": "12345",
+            "especialidade": "Cardio"
+        }
+
+    def test_criar_profissional(self):
+        response = self.client.post(self.url_list, self.dados)
+
+        if response.status_code != 201:
+            print(f"foi para: {response.status_code}")
+
+        if response.status_code == 401:
+            print(f"sou 401: {response.data}")
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Profissional.objects.count(), 1)
+
+    def test_crm_unico(self):
+        """Não deve permitir dois médicos com mesmo CRM"""
+        Profissional.objects.create(**self.dados)
+        response = self.client.post(self.url_list, self.dados) # Tenta criar de novo
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        print(f"Este CRM já existe: {self.dados.get('crm')}")
